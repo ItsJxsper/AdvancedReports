965d9ee830027a821d1bf2f1bee955674f1a1016
ItsJxsper <itsjxsper@outlook.de>
2026-03-03 13:08:23 +0100

    Added MongoDB support to the backend, transitioned the plugin module to Kotlin DSL, and configured additional dependencies and .gitignore entries across modules.

--- discord-bot/build.gradle.kts
+++ discord-bot/build.gradle.kts
@@ -13,6 +13,13 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+
+    compileOnly("org.projectlombok:lombok:1.18.42")
+    annotationProcessor("org.projectlombok:lombok:1.18.42")
+
+    implementation("com.squareup.okhttp3:okhttp:5.3.0")
+
+    implementation("net.dv8tion:JDA:6.3.1")
 }
 
 tasks.test {
