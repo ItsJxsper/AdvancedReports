0af2617c85236d315544030c004752301d153d27
ItsJxsper <itsjxsper@outlook.de>
2026-07-20 22:48:29 +0200

    refactor: simplify mapper logic by removing unnecessary unwrapOptional method and adjusting null checks in the report service

---
+++ common/src/main/java/de/itsjxsper/advancedreports/common/model/player/PlayerDTO.java
@@ -0,0 +1,13 @@
+package de.itsjxsper.advancedreports.common.model.player;
+
+import jakarta.validation.constraints.Size;
+
+import java.io.Serializable;
+import java.util.UUID;
+
+public record PlayerDTO(
+        UUID playerUUID,
+        @Size(message = "Player name must be between 3 and 16 characters", min = 3, max = 16)
+        String playerName
+) implements Serializable {
+}
