4094cedff15f5a93ab634cdf6a58019ef0b6b13b
ItsJxsper <itsjxsper@outlook.de>
2026-07-30 22:42:41 +0200

    fix: update env variable names for GitHub Packages credentials

--- build.gradle.kts
+++ build.gradle.kts
@@ -79,8 +79,8 @@ subprojects {
                 name = "githubPackages"
                 url = uri("https://maven.pkg.github.com/ItsJxsper/advancedreports")
                 credentials {
-                    username = project.findProperty("gpr.user") as String? ?: System.getenv("USERNAME")
-                    password = project.findProperty("gpr.key") as String? ?: System.getenv("TOKEN")
+                    username = project.findProperty("gpr.user") as String? ?: System.getenv("actor")
+                    password = project.findProperty("gpr.key") as String? ?: System.getenv("token")
                 }
             }
         }
