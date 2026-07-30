fecbb32fe35d13d6f4b7e581ee60d4e5fc15e0e6
ItsJxsper <itsjxsper@outlook.de>
2026-07-30 22:37:55 +0200

    feat: add jackson and okhttp dependencies to api module

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -6,10 +6,18 @@ plugins {
 repositories {
     mavenCentral()
     maven("https://repo.papermc.io/repository/maven-public/")
+
 }
 
 dependencies {
+    implementation(libs.common)
+
     implementation(libs.jetbrains.annotations)
+
+    implementation(libs.jackson.databind)
+    implementation(libs.jackson.datatype.jsr310)
+
+    implementation(libs.okhttp)
 
     compileOnly(libs.lombok)
     annotationProcessor(libs.lombok)
