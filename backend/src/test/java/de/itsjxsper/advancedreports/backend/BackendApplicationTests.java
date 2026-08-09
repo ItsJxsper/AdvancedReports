15730a0dd719b94d93c0ba24fa1f9aa4c477b6ba
ItsJxsper <itsjxsper@outlook.de>
2026-08-09 18:07:32 +0200

    test: add unit tests for category service and configure testcontainers setup

---
+++ backend/src/test/java/de/itsjxsper/advancedreports/backend/BackendApplicationTests.java
@@ -0,0 +1,14 @@
+package de.itsjxsper.advancedreports.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.testcontainers.context.ImportTestcontainers;
+
+@ImportTestcontainers(TestcontainersConfiguration.class)
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
